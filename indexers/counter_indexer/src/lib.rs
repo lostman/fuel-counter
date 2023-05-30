ffd695254c3e8f828ac9dfea62c67bcb4372189e
Maciej Woś <maciej.wos@gmail.com>
2023-05-30 17:40:32 +0200

    add ID to IncrEntity

--- indexers/counter_indexer/src/lib.rs
+++ indexers/counter_indexer/src/lib.rs
@@ -5,9 +5,10 @@ use fuel_indexer_plugin::prelude::*;
 #[indexer(manifest = "counter_indexer.manifest.yaml")]
 pub mod counter_indexer_index_mod {
 
-    fn incr_handler(incr: Incr) {
+    fn incr_handler(incr: Incr, block_data: BlockData) {
         Logger::info(&format!("Processing Incr({}).", incr.value));
-        let incr = IncrEntity { value: incr.value };
+        let block_id = first8_bytes_to_u64(block_data.id);
+        let incr = IncrEntity { id: block_id, value: incr.value };
         incr.save();
     }
 
